222b25452ca2a51163034203a72dd983dfddfe02
Scott Hewitt <git@scotthewitt.co.uk>
2012-11-29 14:57:06 +0000

    changed folder name

--- deploy-slime.sh
+++ deploy-slime.sh
@@ -2,8 +2,8 @@
 
 if command -v git >/dev/null; then
     echo Installing through Git
-    mkdir deploy-slime
-    cd deploy-slime
+    mkdir slime-system
+    cd slime-system
     git clone https://github.com/HELOpg/slime-setup
     cd slime-setup
     git submodule foreach 'git fetch origin --tags; git checkout master; git pull' && git pull && git submodule update --init --recursive
